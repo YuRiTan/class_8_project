a98006d2e43123f876921ba51222a67e9d3f7c98
jeromecremers <jerome@xomnia.com>
2018-10-26 12:50:16 +0200

    df as arguments

--- api/blueprints/chat_statistics/statistics.py
+++ api/blueprints/chat_statistics/statistics.py
@@ -10,10 +10,11 @@ return_dict = {'status':0, 'result':{}}
 
 @statistics.route('/whatsapi/api/v0.1/getstatistics')
 def main():
-    result_dict['most_active_users'] = etl.most_active_users()
-    result_dict['most_active_days'] = etl.most_active_days()
-    result_dict['number_of_active_members'] = etl.number_of_active_members()
-    result_dict['number_of_messages'] = etl.number_of_messages()
+    df = etl.data_prepatation()
+    result_dict['most_active_users'] = etl.most_active_users(df)
+    result_dict['most_active_days'] = etl.most_active_days(df)
+    result_dict['number_of_active_members'] = etl.number_of_active_members(df)
+    result_dict['number_of_messages'] = etl.number_of_messages(df)
     return_dict['status'] = 1
     return_dict['result'] = result_dict
     return jsonify(return_dict)
--- api/utils/etl.py
+++ api/utils/etl.py
@@ -2,12 +2,13 @@ import pandas as pd
 import operator
 
 
-df = pd.read_csv('utils/test_df.csv')
-df['timestamp2'] = pd.to_datetime(df['timestamp2'])
-df['weekday'] = df['timestamp2'].dt.weekday_name
+def data_prepatation(df):
+    df = pd.read_csv('utils/test_df.csv')
+    df['timestamp2'] = pd.to_datetime(df['timestamp2'])
+    df['weekday'] = df['timestamp2'].dt.weekday_name
 
 
-def most_active_users():
+def most_active_users(df):
     counts = df.sender.value_counts()
     top_senders = counts.head(5)
     result = top_senders.to_dict()
@@ -15,13 +16,15 @@ def most_active_users():
     return result
 
 
-def most_active_days():
+def most_active_days(df):
     messages_per_day = df.groupby(['weekday']).count().text.to_dict()
     messages_per_day = sorted(messages_per_day.items(), key=operator.itemgetter(1), reverse=True)
     return messages_per_day
 
-def number_of_active_members():
+
+def number_of_active_members(df):
     return df['sender'].unique().shape[0]
 
-def number_of_messages():
+
+def number_of_messages(df):
     return df.shape[0]
