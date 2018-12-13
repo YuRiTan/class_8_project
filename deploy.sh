6f177643bc5026fd0a346f73578adc185d54fda7
Yu Ri Tan <yuri_tan@outlook.com>
2018-12-13 15:34:53 +0100

    removed --rm docker run flag, and updated the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,15 @@
-docker build -t capstone .
-if [ `docker ps | grep capstone | wc -l` -gt 0 ]; then docker stop capstone && docker container rm capstone; fi
-docker run --rm -p 5555:5555 --name capstone -d capstone:latest
+#!/bin/bash
+
+NAME="capstone"
+echo 'Building capstone project...'
+docker build -t $NAME .
+echo 'Done! Successfully build docker image'
+if [ `docker ps -aq --filter name=$NAME | wc -l` -gt 0 ]; then
+    echo 'Found running instance of capstone project:$(docker ps | grep $NAME)'
+    CONTAINERID=$(docker ps -aq --filter name=capstone)
+    docker stop $CONTAINERID && docker rm $CONTAINERID
+    echo 'Stopped and removed docker container($(CONTAINERID), with name: $(NAME))'
+fi
+echo 'Starting new docker container with latest capstone build'
+docker run --rm -p 5555:5555 --name $NAME -d $NAME:latest
+echo 'Done! You should be ready to go by now'
